7d60ef29659cfdeac4416e4a38c7f91301c94742
ppoppy <ppoppy8080@naver.com>
2022-01-23 01:51:47 +0900

    #9012 complete! +1:

--- Data Structure/Baekjoon_#9012.cpp
+++ Data Structure/Baekjoon_#9012.cpp
@@ -5,25 +5,33 @@ int T;
 stack<char> s;
 string c;
 bool flag;
-int main() { 
+
+void init() {
+	flag = false;
+	if (s.empty()) return;
+	while (!s.empty()) s.pop();
+}
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
-	cin>>T;
-	while(T--) {
-		cin>>c;
-		for(int i=0;i<c.size();i++) {
-			if(c[i]=='(') s.push(c[i]);
-			else if(c[i]==')') {
-				if(s.empty()) {
+
+	cin >> T;
+	while (T--) {
+		init();
+		cin >> c;
+		for (int i = 0; i < c.size(); i++) {
+			if (c[i] == '(') s.push('(');
+			else if (c[i] == ')') {
+				if (s.empty()) {
 					flag = true;
 					break;
 				}
 				s.pop();
 			}
 		}
-		if(flag) cout<<"NO\n";
-		else cout<<"YES\n";	
+		if (!s.empty()) flag = true;
+		if (flag) cout << "NO\n";
+		else cout << "YES\n";
 	}
 	return 0;
 }
