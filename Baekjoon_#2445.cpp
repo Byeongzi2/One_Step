05e590cdd0b9da37ac4a46801fcc3f591f95a327
ppoppy <ppoppy8080@naver.com>
2022-01-28 01:27:07 +0900

    #2522 0128

--- Baekjoon_#2445.cpp
+++ Baekjoon_#2445.cpp
@@ -1,37 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-int MAXN;
-void star(int start,int end) {
-	for(int i=start;i<=end;i++) {
-		for(int j=1;j<=i;j++){
-			cout<<'*';
-		}
-		for(int j=0;j<(MAXN+1-2*i);j++) {
-			cout<<' ';
-		}
-		for(int j=1;j<=i;j++) {
-			cout<<'*';
-		}
-		cout<<'\n';
-	}
+int N;
+
+void star(int i) {
+	for(int j=0;j<N-i;j++) cout<<' ';
+	for(int j=0;j<i;j++) cout<<'*';
 }
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int N;
+	
 	cin>>N;
-	MAXN = 2*N-1;
-	star(1,N);
+	for(int i=1;i<=N;i++) {
+		star(i);
+		cout<<'\n';
+	}
 	for(int i=N-1;i>0;i--) {
-		for(int j=1;j<=i;j++){
-			cout<<'*';
-		}
-		for(int j=0;j<(MAXN+1-2*i);j++) {
-			cout<<' ';
-		}
-		for(int j=1;j<=i;j++) {
-			cout<<'*';
-		}
+		star(i);
 		cout<<'\n';
 	}
 	return 0;
