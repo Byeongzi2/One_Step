7cb0616ecb2dd9fb49b5d7747f103357011782eb
ppoppy <ppoppy8080@naver.com>
2022-01-29 23:33:20 +0900

    #10992 starprint

--- Baekjoon_#10992.cpp
+++ Baekjoon_#10992.cpp
@@ -8,15 +8,16 @@ int main() {
 	cin.tie(0);
 	
 	cin>>N;
-	for(int g=1;g<=N;g++) {
+	for(int g=1;g<N;g++) {
 		for(int i=0;i<N-g;i++) {
 			cout<<' ';
 		}
-		for(int i=0;i<(2*g-1);i++) {
-			if(!(i%2)) cout<<'*';
-			else cout<<' '; 
+		for(int i=0;i<2*g-1;i++) {
+			if(i==0||i==2*(g-1)) cout<<'*';
+			else cout<<' ';
 		}
 		cout<<'\n';
-	}	
+	}
+	for(int i=0;i<2*N-1;i++) cout<<'*';	
 	return 0;
 }
