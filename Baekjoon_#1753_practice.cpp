4f3ef082761ce619b3d03d601d1f997df58a488c
ppoppy <ppoppy8080@naver.com>
2022-02-18 23:47:28 +0900

    #1753 practice ver

---
+++ Baekjoon_#1753_practice.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int V,E,K,a,b;
+priority_queue<Data> pq;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin>>V>>E>>K;
+	for(int i=0;i<E;i++) {
+		cin>>a>>b>>w;
+	}
+	
+	return 0;
+}
