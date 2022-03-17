b8b54b9a7b6b2c241e8f88713d65f1c5c79835fc
ppoppy <ppoppy8080@naver.com>
2022-03-17 18:46:02 +0900

    #15649 add ver2.

--- Backtracking/Baekjoon_#15649.cpp
+++ Backtracking/Baekjoon_#15649.cpp
@@ -1,3 +1,39 @@
+// used배열 사용한 버전
+#include<bits/stdc++.h>
+using namespace std;
+#define MAXN 9
+
+int M, N;
+int arr[MAXN];
+bool used[MAXN];
+
+void backtrack(int idx, int pre) {
+	if (idx == N) {
+		for (int i = 0; i < N; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= M; i++) {
+		if (used[i]) continue;
+		arr[idx] = i;
+		used[i] = true;
+		backtrack(idx + 1, i);
+		used[i] = false;
+	}
+
+}
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> M >> N;
+	backtrack(0, 0);
+	return 0;
+} 
+// =========================================================================
+//set 사용한 버전 
 #include<bits/stdc++.h>
 using namespace std;
 #define MAXN 9
