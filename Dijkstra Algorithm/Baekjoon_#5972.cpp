1a945dbad74832bde26ba393fd12aa4154263a1c
ppoppy <ppoppy8080@naver.com>
2022-03-20 00:25:39 +0900

    #5972

---
+++ Dijkstra Algorithm/Baekjoon_#5972.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAXN 50001
+#define INF 987654321
+
+struct Data {
+	int node,weight;
+	Data(){};
+	Data(int node, int weight) : node(node), weight(weight) {};
+	bool operator<(const Data d) const {
+		return weight>d.weight;
+	}
+};
+
+int N,M,A,B,C;
+vector<Data> v[MAXN];
+int weight[MAXN];
+bool visited[MAXN];
+void Dijkstra(int start) {
+	priority_queue<Data> pq;
+	pq.push(Data(start,0));
+	weight[start] = 0;
+	
+	Data curr;
+	while(!pq.empty()) {
+		curr = pq.top();
+		pq.pop();
+		if(visited[curr.node]) continue;
+		visited[curr.node] = true;
+		Data next;
+		for(int i=0;i<v[curr.node].size();i++) {
+			next = v[curr.node].at(i);
+			weight[next.node] = min(weight[next.node] , weight[curr.node]+next.weight);
+			pq.push(Data(next.node,weight[next.node]));
+		}
+	}
+	
+}
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin>>N>>M;
+	for(int i=0;i<M;i++) {
+		cin>>A>>B>>C;
+		v[A].push_back(Data(B,C));
+		v[B].push_back(Data(A,C));
+	}
+	for(int i=0;i<=N;i++) weight[i] = INF;
+	Dijkstra(1);
+	cout<<weight[N];
+	return 0;
+}
+
