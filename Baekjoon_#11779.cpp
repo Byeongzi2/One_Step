86cb9d8bc0beff5a124d9097dac64a078649e6f6
ppoppy <ppoppy8080@naver.com>
2022-03-23 21:20:45 +0900

    #11779 시간초과 해결해야함

--- Baekjoon_#11779.cpp
+++ Baekjoon_#11779.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #define MAXN 1001
 #define ll long long
+#define INF 1e10+1
 struct Data {
 	int node;
 	ll weight;
@@ -15,25 +16,32 @@ struct Data {
 int n,m;
 vector<Data> v[MAXN];
 priority_queue<Data> pq;
-bool visited[MAXN];
-int cost[MAXN];
-
+ll cost[MAXN];
+int parent[MAXN];
+vector<int> path;
 void Dijkstra(int x) {
 	cost[x] = 0;
+	parent[x] = x;
 	pq.push(Data(x,0));
 	Data curr,next;
 	while(!pq.empty()) {
-		curr = pq.front();
+		curr = pq.top();
 		pq.pop();
-		if(visited[curr.node]) continue;
-		visited[curr.node] = true;
 		for(int i=0;i<v[curr.node].size();i++) {
 			next = v[curr.node].at(i);
+			if(cost[next.node]<=cost[curr.node]+next.weight) continue;
+			parent[next.node] = curr.node;
 			pq.push(Data(next.node,cost[next.node]));
-			cost[next.node] = min(cost[next.node],cost[curr.node]+next.weight);
+			cost[next.node] = cost[curr.node]+next.weight;
 		}
 	}
 } 
+void route(int x) {
+	while(parent[x]!=x) {
+		path.push_back(x);
+		x = parent[x];
+	} 
+}
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
@@ -41,12 +49,20 @@ int main() {
 	cin>>n>>m;
 	int a,b;
 	ll w;
+	for(int i=0;i<=n;i++) {
+		cost[i] = INF;
+	}
 	for(int i=0;i<m;i++) {
 		cin>>a>>b>>w;
 		v[a].push_back(Data(b,w));
 	}
 	cin>>a>>b;
 	Dijkstra(a);
-	
+	route(b);
+	cout<<cost[b]<<'\n'<<path.size()+1<<'\n';
+	cout<<a<<" ";
+	for(int i=path.size()-1;i>=0;i--) {
+		cout<<path[i]<<" ";
+	}
 	return 0;
 }
